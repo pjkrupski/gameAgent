8944e164f140b8a4e1b464184e0c3ad2e7af8cf6
willbed34 <william_bednarz@brown.edu>
2024-04-02 13:45:58 -0400

    fixed search line and changed L function

--- 1410AdversarialSearch/searches.py
+++ 1410AdversarialSearch/searches.py
@@ -1,34 +1,52 @@
 
 #look at this
 def search_L(self, c, board, row, col):
-        #Searches for a len 5 L pattern of c's 
-        rows = self._dim
-        cols = self._dim
-        if (row - 2 <= 0 and col + 2 < cols and
-            board[row-1][col] == c and
-            board[row-2][col] == c and
-            board[row][col+1] == c and
-            board[row][col+2] == c):
+        # rows = self._dim
+        # cols = self._dim
+        top_left_corner = [board[0][0], board[1][0], board[0][1]]
+        top_right_corner = [board[0][-1], board[0][-2], board[-1][1]]
+        bottom_left_corner = [board[-1][0], board[-2][0], board[-1][1]]
+        bottom_right_corner = [board[-1][-1], board[-1][-2], board[-2][-1]]
+        
+        if _all_same(top_left_corner, c):
             return True
-        if (row + 2 < rows and col + 2 < cols and
-            board[row + 1][col] == c and
-            board[row + 2][col] == c and
-            board[row][col+1] == c and
-            board[row][col+2] == c):
+        if _all_same(top_right_corner, c):
             return True
-        if (row - 2 <= 0 and col - 2 >= 0 and
-            board[row-1][col] == c and
-            board[row-2][col] == c and
-            board[row][col-1] == c and
-            board[row][col-2] == c):
+        if _all_same(bottom_left_corner, c):
             return True
-        if (row + 2 < rows and col + 2 < cols and
-            board[row + 1][col] == c and
-            board[row + 2][col] == c and
-            board[row][col-1] == c and
-            board[row][col-2] == c):
+        if _all_same(bottom_right_corner, c):
             return True
         return False
+        #Searches for a len 5 L pattern of c's 
+        # rows = self._dim
+        # cols = self._dim
+        # if (row - 2 <= 0 and col + 2 < cols and
+        #     board[row-1][col] == c and
+        #     board[row-2][col] == c and
+        #     board[row][col+1] == c and
+        #     board[row][col+2] == c):
+        #     return True
+        # if (row + 2 < rows and col + 2 < cols and
+        #     board[row + 1][col] == c and
+        #     board[row + 2][col] == c and
+        #     board[row][col+1] == c and
+        #     board[row][col+2] == c):
+        #     return True
+        # if (row - 2 <= 0 and col - 2 >= 0 and
+        #     board[row-1][col] == c and
+        #     board[row-2][col] == c and
+        #     board[row][col-1] == c and
+        #     board[row][col-2] == c):
+        #     return True
+        # if (row + 2 < rows and col + 2 < cols and
+        #     board[row + 1][col] == c and
+        #     board[row + 2][col] == c and
+        #     board[row][col-1] == c and
+        #     board[row][col-2] == c):
+        #     return True
+        # return False
+
+        #searches for L in the corners
 
 #TODO
 def search_V():
@@ -77,24 +95,24 @@ def search_Line(self, c, board, row, col):
     # bind the output of _all_same for diagonal1 for its two uses
     asd1 = _all_same(diagonal1, c)
     if asd1:  # #onlyinpython / #imissoptions
-        return asd1
+        return True
 
     diagonal2 = [board[i][self._dim - 1 - i] for i in range(self._dim)]
     asd2 = _all_same(diagonal2, c)
     if asd2:
-        return asd2
+        return True
 
     for row in board:
         asr = _all_same(row, c)
         if asr:
-            return asr
+            return True
 
     for c in range(self._dim):
         # why oh why didn't I just use numpy arrays?
         col = [board[r][c] for r in range(self._dim)]
         asc = _all_same(col, c)
         if asc:
-            return asc
+            return True
     return False
 
     
